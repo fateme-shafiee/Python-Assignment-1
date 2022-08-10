cec2380e56fbb08d1277c80cebe651f3cc12ec42
fateme-shafiee <110778243+fateme-shafiee@users.noreply.github.com>
2022-08-10 21:55:37 +0430

    Add files via upload

---
+++ cylinder-shafiee.py
@@ -0,0 +1,10 @@
+#Ex5
+Radius=float(input('Enter the radius of the cylinder:'))
+Height=float(input('Enter the height of the cylinder'))
+pi=3.14
+Side_area=(2*pi*Radius*Height)
+cylinder_volume=(pi*Radius*Radius*Height)
+Total_area=(Side_area)+(2*pi*Radius*Radius)
+print('area of the cylinder=',Side_area )
+print('cylinder volume=',cylinder_volume )
+print('cylinder area=',Total_area)
